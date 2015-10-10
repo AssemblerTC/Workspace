bdb27e2165ab61471b3515c4a1684e5bdf2238d6
assemblerTC <assemblerTC@gmail.com>
2015-10-10 12:27:47 -0400

    my template base

---
+++ CONFIG/template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef ACMTUYO
+struct RTC{~RTC(){cerr << "Time: " << clock() * 1.0 / CLOCKS_PER_SEC <<" seconds\n";}} runtimecount;
+#define DBG(X) cerr << #X << " = " << X << '\n';
+#else
+struct RTC{};
+#define DBG(X)
+#endif
+
+#define fast_io() ios_base::sync_with_stdio(false)
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+#define sz(x) ((int)(x).size())
+#define all(c) (c).begin(),(c).end()
+#define forn(i, n) for (int i = 0; i < (n); i++)
+typedef long long int number;
+
+int main() {
+
+  return 0;
+}
